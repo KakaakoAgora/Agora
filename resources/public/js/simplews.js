a8dffcd9bcb1b1c43c3ac7ab0d166b2d651a7dd7
Kyle Oba <koba@mudphone.com>
2014-06-21 12:56:11 -1000

    Add sample websocket client in javascript.

---
+++ resources/public/js/simplews.js
@@ -0,0 +1,32 @@
+// (def ws-url "ws://localhost:3000/agora-socket")
+// (def ws (new js/WebSocket ws-url))
+var wsUri = "ws://localhost:3000/agora-socket";
+
+// Let us open a web socket
+var ws = new WebSocket(wsUri);
+var lastMsg;
+
+ws.onopen = function()
+{
+  // Web Socket is connected, send data using send()
+  var pollingOn = {'msg': 'start polling', 
+                   'type': edn.keyword('poll'), 
+                   'name': 'generic js module'}
+  var msg = edn.stringify(pollingOn);
+  ws.send(msg);
+  console.log("message sent:");
+  console.log(msg);
+};
+
+ws.onmessage = function (evt) 
+{ 
+  var received_msg = edn.valueOf(edn.parse(evt.data));
+  console.log("received msg:");
+  console.log(received_msg);
+};
+
+ws.onclose = function()
+{ 
+  // websocket is closed.
+  console.log("Connection is closed..."); 
+};

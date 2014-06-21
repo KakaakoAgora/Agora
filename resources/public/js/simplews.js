c35c29f7fc71c7ad6dcc47faae2a1b5a3d33c312
Kyle Oba <koba@mudphone.com>
2014-06-21 13:14:00 -1000

    Allow sending mark-point messages to the Agora framework.

--- resources/public/js/simplews.js
+++ resources/public/js/simplews.js
@@ -1,30 +1,55 @@
-var wsUri = "ws://localhost:3000/agora-socket";
+$(function() {
+  var wsUri = "ws://localhost:3000/agora-socket";
 
-// Let us open a web socket
-var ws = new WebSocket(wsUri);
+  // Let us open a web socket
+  var ws = new WebSocket(wsUri);
 
-ws.onopen = function()
-{
-  // Web Socket is connected, send data using send()
-  console.log("Connection open...");
-  var pollingOn = {'msg': 'start polling', 
-                   'type': edn.keyword('poll'), 
-                   'name': 'generic js module'}
-  var msg = edn.stringify(pollingOn);
-  ws.send(msg);
-  console.log("message sent:");
-  console.log(msg);
-};
+  ws.onopen = function()
+  {
+    // Web Socket is connected, send data using send()
+    console.log("Connection open...");
+    var pollingOn = {'msg': 'start polling', 
+                     'type': edn.keyword('poll'), 
+                     'name': 'generic js module'}
+    var msg = edn.stringify(pollingOn);
+    ws.send(msg);
+    console.log("message sent:");
+    console.log(msg);
+  };
 
-ws.onmessage = function (evt) 
-{ 
-  var received_msg = edn.valueOf(edn.parse(evt.data));
-  console.log("received msg:");
-  console.log(received_msg);
-};
+  ws.onmessage = function (evt) 
+  { 
+    var received_msg = edn.valueOf(edn.parse(evt.data));
+    console.log("received msg:");
+    console.log(received_msg);
+  };
 
-ws.onclose = function()
-{ 
-  // websocket is closed.
-  console.log("Connection is closed..."); 
-};
+  ws.onclose = function()
+  { 
+    // websocket is closed.
+    console.log("Connection is closed..."); 
+  };
+
+  function sendPoint(_x, _y, _magnitude) {
+    var x = parseInt(_x);
+    var y = parseInt(_y);
+    var mag = parseFloat(_magnitude);
+    var markPointMsg = edn.stringify({
+                        'msg': 'mark-point',
+                        'type': edn.keyword('update'),
+                        'x': x,
+                        'y': y,
+                        'magnitude': mag
+                      });
+    console.log("Sending msg:");
+    console.log(markPointMsg);
+    ws.send(markPointMsg);
+  };
+
+  $( "#markPointButton" ).click(function() {
+    var x = $('#xVal').val();
+    var y = $('#yVal').val();
+    var mag = $('#magnitude').val();
+    sendPoint(x, y, mag);
+  });
+});
